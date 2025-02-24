8161c35a4f9c031d1dff445eeaf39042b10b7ca2
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-24 17:11:08 -0300

    First commit, base project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>
+        Grid Host:
+      </h2>
+      <h2>
+        Connected Profile: 
+      </h2>
+    </>
+  )
+}
+
+export default App
