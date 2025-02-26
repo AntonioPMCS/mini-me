dce7768b6a774ea6dfcbe43b5a9677904808a59e
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-26 19:31:37 -0300

    Cleaned up App componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import './App.css'
 import UpProviderContext from './context/UpProviderContext';
-
-// Import the LUKSO web-components library
-let promise: Promise<unknown> | null = null;
-if (typeof window !== "undefined") {
-  promise = import("@lukso/web-components");
-}
 
 function App() {
   // Track connected accounts
@@ -16,19 +10,6 @@ function App() {
     walletConnected,
     chainId
   } = useContext(UpProviderContext)
-
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // Load web component here if needed
-    promise?.then(() => {
-      setMounted(true);
-    });
-  }, []);
-
-  if (!mounted) {
-    return null; // or a loading placeholder
-  }
 
   return (
     <>
