a6646f71f7c7e18e6dee0e9d37b3bc522c31e459
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-27 00:01:39 -0300

    NO message

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { useContext } from 'react';
 import './App.css'
-import UpProviderContext from './context/UpProviderContext';
+import { useContext } from 'react'
+import UpProviderContext from './context/UpProviderContext'
 
 function App() {
-  // Track connected accounts
-  const {
+   // Track connected accounts
+   const {
     accounts, 
     contextAccounts, 
     walletConnected,
     chainId
   } = useContext(UpProviderContext)
 
+
   return (
-    <>
+    <div className="bg-white text-2xl flex flex-col items-center justify-center w-full mx-0 ">
+      <h2>
+        Inside the view
+      </h2>
       <h2>
         chainId: {chainId}
       </h2>
       <h2>
-        Grid Host (Context account): {contextAccounts[0]}
+        Context Address (Grid host): {contextAccounts[0]}
       </h2>
-      <h2>
-        Connected Profile: {walletConnected? 'True' : 'False'} - {accounts[0]}
-      </h2>
-    </>
+    </div>
   )
 }
 
