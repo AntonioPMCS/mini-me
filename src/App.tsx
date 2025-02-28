b61fc7307881104d35a1ea5055233a581b36434d
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2025-02-28 11:40:55 -0300

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import './styles/App.css'
-import { useContext } from 'react'
-import UpProviderContext from './context/UpProviderContext'
 import Chat from './components/Chat'
 import StatusBar from './components/StatusBar'
 
 function App() {
-   // Track connected accounts
-   const {
-    accounts, 
-    contextAccounts, 
-    walletConnected,
-    chainId
-  } = useContext(UpProviderContext)
-
 
   return (
     <div className="app-container">
